38d7cb3ddfba06b874d8ef7f3ee0cc432dffe98b
Jeriel Martinez <martinez.jeriel01@gmail.com>
2025-01-03 21:20:10 -0500

    feat: enhance Home page with mileage display, progress bar, and bottom navigation

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,23 +1,49 @@
+// src/pages/Home/Home.jsx
+import React from 'react';
+import './Home.css';
+
 function Home() {
-    return (
-      <div className="home-container">
-        <div className="shoe-circle">
-          {/* Shoe icon or image */}
-        </div>
-  
-        <div className="mileage-info">
-          <p className="mileage-text">150 / 250</p>
-          <div className="mileage-progress">
-            {/* Progress bar goes here */}
-          </div>
-        </div>
-  
-        <button className="add-button">+</button>
-  
-        <nav className="bottom-nav">
-          {/* Icons for Past Runs, Home, Collection, Upcoming */}
-        </nav>
+  // For the sake of the example, let's hardcode mileage data
+  const currentMiles = 150;
+  const maxMiles = 250;
+
+  // Calculate percentage for the progress bar
+  const progressPercent = (currentMiles / maxMiles) * 100;
+
+  return (
+    <div className="home-container">
+      
+      {/* Top Circle with Shoe */}
+      <div className="shoe-circle">
+        <img src="/images/shoe.png" alt="Shoe" className="shoe-img" />
       </div>
-    );
-  }
-  
+
+      {/* Mileage Display & Progress Bar */}
+      <div className="mileage-info">
+        <p className="mileage-text">
+          {currentMiles} / {maxMiles}
+        </p>
+        <div className="mileage-progress">
+          <div
+            className="mileage-bar"
+            style={{ width: `${progressPercent}%` }}
+          ></div>
+        </div>
+      </div>
+
+      {/* Floating "+" Button */}
+      <button className="add-button">+</button>
+
+      {/* Bottom Navigation */}
+      <nav className="bottom-nav">
+        <div className="nav-item"> {/* Icon for Past Runs */}</div>
+        <div className="nav-item"> {/* Icon for Home */}</div>
+        <div className="nav-item"> {/* Icon for Collection/Wants */}</div>
+        <div className="nav-item"> {/* Icon for Upcoming */}</div>
+      </nav>
+
+    </div>
+  );
+}
+
+export default Home;
